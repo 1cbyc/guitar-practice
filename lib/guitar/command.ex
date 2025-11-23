a5d8f2e932ba556e047ae765a1c4a13e27ab2b90
1cbyc <ei@nsisong.com>
2025-11-23 15:18:32 +0100

    fix(lib/guitar): Update command.ex

--- lib/guitar/command.ex
+++ lib/guitar/command.ex
@@ -1,4 +1,8 @@
 defmodule Guitar.Command do
+  @moduledoc """
+  Behaviour for guitar practice commands
+  """
+
   @type options :: [{atom(), term}]
   @type entries :: [Guitar.Log.Entry]
   @type storage :: Guitar.Storage.t()
